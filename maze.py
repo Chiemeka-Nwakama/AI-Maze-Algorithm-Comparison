246ef5fc6d8bf0c0e508162477ff2150a22a504a
nwaka013 <nwaka013@umn.edu>
2023-12-04 13:51:50 -0600

    Update maze.py
    generate maze function not sure if it works tbh

--- maze.py
+++ maze.py
@@ -97,6 +97,65 @@ class maze:
         s_prev = s_curr
         # print(self.unbacktracked)
         return a
+    
+    
+    import random
+
+def generate_maze(x):
+    # Initialize maze with all walls
+    maze = {(i, j): ['U', 'D', 'L', 'R'] for i in range(x) for j in range(x)}
+    
+    # Randomly choose a starting point
+    start = (random.randint(0, x - 1), random.randint(0, x - 1))
+    maze[start] = []
+
+    # List of visited cells
+    visited = [start]
+    
+    while visited:
+        current = visited[-1]
+        
+        # Get neighbors of the current cell
+        neighbors = [
+            (current[0] - 1, current[1]),  # Up
+            (current[0] + 1, current[1]),  # Down
+            (current[0], current[1] - 1),  # Left
+            (current[0], current[1] + 1)   # Right
+        ]
+        
+        # Shuffle the neighbors randomly
+        random.shuffle(neighbors)
+        
+        found = False
+        for neighbor in neighbors:
+            if 0 <= neighbor[0] < x and 0 <= neighbor[1] < x and neighbor not in visited:
+                # Remove the wall between the current cell and the neighbor
+                if neighbor[0] < current[0]:
+                    maze[current].remove('U')
+                    maze[neighbor].remove('D')
+                elif neighbor[0] > current[0]:
+                    maze[current].remove('D')
+                    maze[neighbor].remove('U')
+                elif neighbor[1] < current[1]:
+                    maze[current].remove('L')
+                    maze[neighbor].remove('R')
+                elif neighbor[1] > current[1]:
+                    maze[current].remove('R')
+                    maze[neighbor].remove('L')
+                
+                visited.append(neighbor)
+                found = True
+                break
+        
+        if not found:
+            visited.pop()
+    
+    # Choose a random goal position at the end of the path
+    goal = visited[-1]
+    
+    return maze, goal
+
+
 
 def main():
     loc = (0,0)
